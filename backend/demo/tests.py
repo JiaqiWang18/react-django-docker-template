dbe778829784d0ecabe6fd41cc49e09ab806fcb0
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2021-10-24 17:05:59 -0400

    create backend test

--- backend/demo/tests.py
+++ backend/demo/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from demo.models import Note
 
-# Create your tests here.
+class NoteTestCase(APITestCase):
+    def test_create_note(self):
+        """
+        Ensure we can create a new Note.
+        """
+        url = reverse('note-list')
+        data = {'content': 'new note'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Note.objects.count(), 1)
+        self.assertEqual(Note.objects.filter().first().content, 'new note')
+    
+    def test_delete_note(self):
+        """
+        Ensure we can delete a Note.
+        """
+        obj = Note(content="new note")
+        obj.save()
+        url = reverse('note-detail', kwargs={'pk': obj.pk})
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Note.objects.count(), 0)
